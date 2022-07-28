4bb6a93cb5256c2c8fbe31218fd5390f507b9b55
Arthur <timeiqu@gmail.com>
2022-07-28 23:53:45 +0300

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,8 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['♦', '♠', '♥', '♣']
 
-puts "#{values.sample}#{suits.sample}"
+samples = values.sample(2)
+
+samples.each do |value|
+  puts "#{value}#{suits.sample}"
+end
